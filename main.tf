73ee3b1042775bc88fe401969fd5dd101cbf1a2a
poroping <43107292+poroping@users.noreply.github.com>
2021-07-23 12:17:14 +0000

    add features

--- main.tf
+++ main.tf
@@ -1,10 +1,51 @@
 /**
  * # terraform-fortios-sslvpn
  * 
+ * Requires forked version of fortios provider
+ * 
+ * Will create SSLVPN portal and settings for split and optionally full tunnel.
+ * 
+ * Firewall policies will need to be created manually.
+ * 
  * ## Usage:
  *
- * ### Example of 'terraform-fortios-sslvpn' module.
+ * ### Basic example of 'terraform-fortios-sslvpn' module.
  *
+ *
+ * ```hcl
+ * terraform {
+ *   required_version = ">= 1.0.1"
+ *   backend "local" {}
+ *   required_providers {
+ *     fortios = {
+ *       source  = "poroping/fortios"
+ *       version = "~> 2.0.0"
+ *     }
+ *   }
+ * }
+ * 
+ * provider "fortios" {
+ *   hostname = var.hostname
+ *   token    = var.token
+ *   vdom     = "root"
+ *   insecure = "true"
+ *   #   cabundlefile =  "/path/yourCA.crt"
+ * }
+ * 
+ * 
+ * data "fortios_system_interface" "wan" {
+ *   name = "wan1"
+ * }
+ * 
+ * module "sslvpn" {
+ *   source  = "poroping/sslvpn/fortios"
+ *   version = "0.2.0"
+ * 
+ *   ssl_interface    = data.fortios_system_interface.wan
+ * }
+ * ```
+ * 
+ * ### Example of 'terraform-fortios-sslvpn' module.
  *
  *
  * ```hcl
@@ -46,13 +87,17 @@
  * }
  * 
  * module "sslvpn" {
- *   source = "github.com/poroping/terraform-fortios-sslvpn"
+ *   source  = "poroping/sslvpn/fortios"
+ *   version = "0.2.0"
  * 
  *   prefix           = fortios_system_vdom.vdom.name
  *   ssl_interface    = fortios_system_interface.loopsmcgee
  *   outside_int_name = fortios_system_interface.ext.name
  *   vdom             = fortios_system_vdom.vdom.name
  *   dns_servers      = ["192.168.1.1", "192.168.2.1"]
+ *   has_full_tunnel  = true
+ *   https_redirect   = "disable"
+ *   port             = 9443
  * }
  * ```
  *
@@ -84,6 +129,8 @@ resource "fortios_firewall_address" "sslvpn_split_user_pool" {
 }
 
 resource "fortios_firewall_address" "sslvpn_full_user_pool" {
+  count = var.has_full_tunnel ? 1 : 0
+
   vdomparam    = var.vdom
   allow_append = true
 
@@ -147,6 +194,8 @@ resource "fortios_vpnsslweb_portal" "split_tunnel" {
 }
 
 resource "fortios_vpnsslweb_portal" "full_tunnel" {
+  count = var.has_full_tunnel ? 1 : 0
+
   vdomparam    = var.vdom
   allow_append = true
 
@@ -163,7 +212,7 @@ resource "fortios_vpnsslweb_portal" "full_tunnel" {
   # if either keep_alive or auto_connect enabled this needs to be enabled
 
   ip_pools {
-    name = fortios_firewall_address.sslvpn_full_user_pool.name
+    name = fortios_firewall_address.sslvpn_full_user_pool[0].name
   }
 
   lifecycle {
@@ -172,27 +221,31 @@ resource "fortios_vpnsslweb_portal" "full_tunnel" {
 }
 
 resource "fortios_vpnsslweb_realm" "full_tunnel" {
+  count = var.has_full_tunnel ? 1 : 0
+
   vdomparam = var.vdom
 
   url_path = "full-tunnel"
 }
 
 resource "fortios_vpnssl_settings" "settings" {
-  vdomparam = var.vdom
+  dynamic_sort_subtable = true
+  vdomparam             = var.vdom
 
   algorithm           = "high"
   auth_timeout        = 43200
   default_portal      = fortios_vpnsslweb_portal.blackhole.name
-  https_redirect      = "enable"
+  dtls_tunnel         = "enable"
+  https_redirect      = var.https_redirect
   idle_timeout        = 300
   login_attempt_limit = 5
   login_block_time    = 90
   login_timeout       = 90
-  port                = 443
+  port                = var.port
   port_precedence     = "enable"
   ssl_min_proto_ver   = "tls1-2"
 
-  servercert  = var.ssl_servercert_name == null ? null : var.ssl_servercert_name
+  servercert  = var.ssl_servercert_name == "" ? null : var.ssl_servercert_name
   dns_server1 = try(var.dns_servers[0], null)
   dns_server2 = try(var.dns_servers[1], null)
   dns_suffix  = join(",", var.dns_suffixes)
@@ -205,31 +258,16 @@ resource "fortios_vpnssl_settings" "settings" {
     name = var.ssl_interface.name
   }
 
-  tunnel_ip_pools {
-    name = fortios_firewall_address.sslvpn_full_user_pool.name
+  dynamic "tunnel_ip_pools" {
+    for_each = var.has_full_tunnel ? ["1"] : []
+
+    content {
+      name = fortios_firewall_address.sslvpn_full_user_pool[0].name
+    }
   }
 
   tunnel_ip_pools {
     name = fortios_firewall_address.sslvpn_split_user_pool.name
-  }
-
-  authentication_rule {
-    id     = 1
-    portal = fortios_vpnsslweb_portal.split_tunnel.name
-
-    groups {
-      name = fortios_user_group.sslvpn_users.name
-    }
-  }
-
-  authentication_rule {
-    id     = 2
-    portal = fortios_vpnsslweb_portal.full_tunnel.name
-    realm  = fortios_vpnsslweb_realm.full_tunnel.url_path
-
-    groups {
-      name = fortios_user_group.sslvpn_users.name
-    }
   }
 }
 
@@ -245,18 +283,35 @@ resource "fortios_vpnssl_settings_authentication_rule" "split" {
 }
 
 resource "fortios_vpnssl_settings_authentication_rule" "full" {
+  count = var.has_full_tunnel ? 1 : 0
+
   vdomparam = var.vdom
-  portal    = fortios_vpnsslweb_portal.full_tunnel.name
-  realm     = fortios_vpnsslweb_realm.full_tunnel.url_path
+
+  portal = fortios_vpnsslweb_portal.full_tunnel[0].name
+  realm  = fortios_vpnsslweb_realm.full_tunnel[0].url_path
 
   groups {
     name = fortios_user_group.sslvpn_users.name
   }
 }
 
-resource "fortios_firewall_policy" "vpn_loopback_in" {
+resource "fortios_firewallservice_custom" "custom_sslvpn_port" {
+  count = var.port == 443 ? 0 : 1
+
   vdomparam = var.vdom
-  count     = var.ssl_interface.type == "loopback" ? 1 : 0
+
+  name          = "SSLVPN-HTTPS-CUSTOM"
+  category      = "Remote Access"
+  protocol      = "TCP/UDP/SCTP"
+  tcp_portrange = var.port
+  visibility    = "enable"
+
+}
+
+resource "fortios_firewall_policy" "vpn_loopback_in" {
+  count = var.ssl_interface.type == "loopback" ? 1 : 0
+
+  vdomparam             = var.vdom
 
   action     = "accept"
   logtraffic = "utm"
@@ -272,15 +327,19 @@ resource "fortios_firewall_policy" "vpn_loopback_in" {
   }
 
   service {
-    name = "HTTP"
-  }
-
-  service {
-    name = "HTTPS"
-  }
-
-  service {
     name = "PING"
+  }
+
+  service {
+    name = var.port == 443 ? "HTTPS" : fortios_firewallservice_custom.custom_sslvpn_port[0].name
+  }
+
+  dynamic "service" {
+    for_each = var.https_redirect == "enable" ? ["1"] : []
+
+    content {
+      name = "HTTP"
+    }
   }
 
   srcaddr {
@@ -290,10 +349,12 @@ resource "fortios_firewall_policy" "vpn_loopback_in" {
   srcintf {
     name = var.outside_int_name
   }
+
 }
 
 resource "fortios_firewall_address" "dns_servers" {
-  for_each     = var.dns_servers
+  for_each = var.dns_servers
+
   vdomparam    = var.vdom
   allow_append = true
 
@@ -307,9 +368,19 @@ resource "fortios_firewall_address" "dns_servers" {
   }
 }
 
+data "fortios_json_generic_api" "routecheck" {
+  count = length(var.dns_servers) == 0 ? 0 : 1
+
+  path          = "/api/v2/monitor/router/lookup"
+  vdomparam     = var.vdom
+  specialparams = "destination=${try(var.dns_servers[0], "0.0.0.0")}"
+}
+
 resource "fortios_firewall_policy" "sslvpn_access_dns" {
-  vdomparam = var.vdom
-  count     = length(var.dns_servers) == 0 ? 0 : 1
+  count = length(var.dns_servers) == 0 ? 0 : 1
+
+  dynamic_sort_subtable = true
+  vdomparam             = var.vdom
 
   action     = "accept"
   logtraffic = "utm"
@@ -324,7 +395,7 @@ resource "fortios_firewall_policy" "sslvpn_access_dns" {
   }
 
   dstintf {
-    name = "any" # do a lookup
+    name = "any" # try(jsondecode(data.fortios_json_generic_api.routecheck[0].response).results["interface"], "any") #this works but on second apply
   }
 
   service {
@@ -336,11 +407,15 @@ resource "fortios_firewall_policy" "sslvpn_access_dns" {
   }
 
   srcaddr {
-    name = fortios_firewall_address.sslvpn_full_user_pool.name
+    name = fortios_firewall_address.sslvpn_split_user_pool.name
   }
 
-  srcaddr {
-    name = fortios_firewall_address.sslvpn_split_user_pool.name
+  dynamic "srcaddr" {
+    for_each = var.has_full_tunnel ? ["1"] : []
+
+    content {
+      name = fortios_firewall_address.sslvpn_full_user_pool[0].name
+    }
   }
 
   groups {
@@ -365,9 +440,11 @@ resource "fortios_router_static" "sslvpn_splitpool_null_route" {
 }
 
 resource "fortios_router_static" "sslvpn_fullpool_null_route" {
+  count = var.has_full_tunnel ? 1 : 0
+
   vdomparam = var.vdom
 
   blackhole = "enable"
-  dstaddr   = fortios_firewall_address.sslvpn_full_user_pool.name
+  dstaddr   = fortios_firewall_address.sslvpn_full_user_pool[0].name
   status    = "enable"
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,25 @@
-variable "split_address_pool" {
-  description = "IPv4 address pool for split tunnel."
+variable "dns_servers" {
+  description = "Set of IPv4 DNS servers. Only two will be used."
+  type        = set(string)
+  default     = []
+}
+
+variable "dns_suffixes" {
+  description = "Set of DNS suffixes."
+  type        = set(string)
+  default     = []
+}
+
+variable "has_full_tunnel" {
+  description = "Setup an second realm with full tunnel enabled."
+  type        = bool
+  default     = false
+}
+
+variable "https_redirect" {
+  description = "Redirect of port 80 to SSL-VPN port."
   type        = string
-  default     = "10.255.255.0/25"
+  default     = "enable"
 }
 
 variable "full_address_pool" {
@@ -10,10 +28,28 @@ variable "full_address_pool" {
   default     = "10.255.255.128/25"
 }
 
+variable "outside_int_name" {
+  type        = string
+  description = "Name of outside interface if SSLVPN portal not facing internet, ex. enabled on a Loopback."
+  default     = ""
+}
+
+variable "port" {
+  description = "SSLVPN portal port."
+  type        = number
+  default     = 443
+}
+
 variable "prefix" {
   description = "Used to prefix all object names."
   type        = string
   default     = "TF"
+}
+
+variable "split_address_pool" {
+  description = "IPv4 address pool for split tunnel."
+  type        = string
+  default     = "10.255.255.0/25"
 }
 
 variable "ssl_interface" {
@@ -24,28 +60,10 @@ variable "ssl_interface" {
   })
 }
 
-variable "outside_int_name" {
-  type        = string
-  description = "Name of outside interface if SSLVPN portal not facing internet, ex. enabled on a Loopback."
-  default     = ""
-}
-
-variable "dns_suffixes" {
-  description = "Set of DNS suffixes."
-  type        = set(string)
-  default     = []
-}
-
-variable "dns_servers" {
-  description = "Set of IPv4 DNS servers. Only two will be used."
-  type        = set(string)
-  default     = []
-}
-
 variable "ssl_servercert_name" {
   description = "Name of certificate object to use on interface."
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "vdom" {
